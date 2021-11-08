0ce3f4f8c9eae150e03056fa4446f2b6bd6c0702
Nicholas Weidman <weidmannc@comcast.net>
2021-11-08 16:36:02 -0500

    Implemented database_loader with load_database() and save_database() to load files and save files.

---
+++ database_loader.py
@@ -0,0 +1,24 @@
+dic={}
+def load_database(filename):
+    myfile = open(filename, "a")
+    myfile.close()
+    with open(filename, 'r') as f:
+        for line in f:
+            [user, singers] = line.strip().split(":")
+            singersList = singers.split(",")
+            dic[user] = singersList
+    return dic
+    
+
+def save_database(dic, filename):
+    keyList=list(dic.keys())
+    #keyList = sort(keyList)
+    myfile = open(filename, 'w')
+    for item in keyList:
+        string = item + ":"
+        for s in dic[item]:
+            string+= s + ','
+        string=string[:-1]+"\n"
+        myfile.write(string)
+    
+        
